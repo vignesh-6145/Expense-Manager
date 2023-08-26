b4cb05c73a847d076452469b5308628b02c68f27
vignesh.bandla <vignesh.bandla01@gmail.com>
2023-08-26 22:41:16 +0530

    Project Initialiation

---
+++ ExpenseManager/src/main/java/com/vignesh/ExpenseManager/ExpenseManagerApplication.java
@@ -0,0 +1,13 @@
+package com.vignesh.ExpenseManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseManagerApplication.class, args);
+	}
+
+}

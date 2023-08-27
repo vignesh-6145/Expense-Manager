745e034548ffa0b7da1bb50ef0a7c5be0d60bef2
vignesh.bandla <vignesh.bandla01@gmail.com>
2023-08-27 12:00:04 +0530

    feat : added Get Specific user Functionality made certain changes for hyperlinks

--- ExpenseManager/src/main/java/com/vignesh/ExpenseManager/User/UserController.java
+++ ExpenseManager/src/main/java/com/vignesh/ExpenseManager/User/UserController.java
@@ -2,14 +2,33 @@ package com.vignesh.ExpenseManager.User;
 
 import java.util.List;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.websocket.server.PathParam;
 
 @RestController
 public class UserController {
 
 	private UserRepository repository;
+	
+	public User createUser(User user){
+		int userId = user.getUserId();
+		Link selfLink = linkTo(UserController.class).slash(userId).withSelfRel();
+		Link allUsersLink = linkTo(methodOn(UserController.class).getAllUsers()).withRel("all-users");
+		
+		user.add(selfLink);
+		user.add(allUsersLink);
+		return user;
+	}
 	
 	@Autowired
 	public UserController(UserRepository repository) {
@@ -17,7 +36,15 @@ public class UserController {
 	}
 	
 	@GetMapping(path="/users")
-	public List<User> getAllUsers(){
-		return repository.findAll();
+	public CollectionModel<User> getAllUsers(){
+		List<User> users = repository.findAll().stream().map(user -> createUser(user)).toList();
+//		users.replaceAll(this::createUser);	// appending links to each user one by one 
+		CollectionModel<User> usersWithHyperlinks = CollectionModel.of(users);
+		return usersWithHyperlinks;
+	}
+	
+	@GetMapping(path="/users/{userId}")
+	public User getUser(@PathVariable int userId ) {
+		return createUser(repository.findById(userId).get());
 	}
 }

db9e779763035a14c4784da857195394bf036031
vignesh.bandla <vignesh.bandla01@gmail.com>
2023-08-30 19:41:55 +0530

    UPDATE : Added a REST API(POST) call for adding a new expense for a particular user

--- ExpenseManager/src/main/java/com/vignesh/ExpenseManager/User/UserController.java
+++ ExpenseManager/src/main/java/com/vignesh/ExpenseManager/User/UserController.java
@@ -8,21 +8,27 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.Link;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vignesh.ExpenseManager.Exceptions.UserNotFoundException;
 import com.vignesh.ExpenseManager.Expense.Expense;
+import com.vignesh.ExpenseManager.Expense.ExpenseRepository;
+
+import jakarta.validation.Valid;
 
 @RestController
 public class UserController {
-
+	
 	private UserRepository userRepository;
+	private ExpenseRepository expenseRepository;
 	
 	public User createUser(User user){
 		int userId = user.getUserId();
@@ -33,9 +39,21 @@ public class UserController {
 		return user;
 	}
 	
+	public EntityModel<Expense> createExpense(Expense expense){
+		EntityModel<Expense> expenseL = EntityModel.of(expense);
+		int expenseId = expenseL.getContent().getId();
+		int userId = expenseL.getContent().getUser().getUserId();
+		Link selfLink = linkTo(UserController.class).slash(userId).slash("expenses").slash(expenseId).withSelfRel();
+		Link crrUser = linkTo(methodOn(UserController.class).getUser(userId)).withRel("user");
+		Link allExpensesLink = linkTo(UserController.class).slash(userId).slash("expenses").withRel("all-expenses");
+		expenseL.add(crrUser,selfLink,allExpensesLink);
+		return expenseL;
+	}
+	
 	@Autowired
-	public UserController(UserRepository userRepository) {
+	public UserController(UserRepository userRepository,ExpenseRepository expenseRepository) {
 		this.userRepository = userRepository;
+		this.expenseRepository = expenseRepository;
 	}
 	
 	@GetMapping(path="/users")
@@ -55,12 +73,24 @@ public class UserController {
 	}
 	
 	@GetMapping(path="/users/{userId}/expenses")
-	public ResponseEntity<List<Expense>> getUserExpense(@PathVariable int userId){
+	public ResponseEntity<CollectionModel> getUserExpense(@PathVariable int userId){
 		Optional<User> user = userRepository.findById(userId);
 		System.out.println(user.get().getExpenses());
 		if(user.isEmpty())
 			throw new UserNotFoundException(String.format("User with Id : %d not found in our records", userId));
-		return new ResponseEntity<List<Expense>> (user.get().getExpenses(),HttpStatus.OK);
 		
+//		return new ResponseEntity<List<En>> (,HttpStatus.OK);
+		return new ResponseEntity<CollectionModel>(CollectionModel.of(user.get().getExpenses().stream().map(expense -> createExpense(expense)).toList()),HttpStatus.OK);
+	}
+	@PostMapping(path="/users/{userId}/addExpense")
+	public ResponseEntity addUserExpense(@PathVariable int userId, @Valid @RequestBody Expense expense) {
+		Optional<User> user = userRepository.findById(userId);
+		if(user.isEmpty())
+			throw new UserNotFoundException(String.format("User with Id : %d not found in our records", userId));
+		expense.setUser(user.get());
+		expense = expenseRepository.save(expense);
+//		System.out.println();
+		
+		return ResponseEntity.created(linkTo(methodOn(UserController.class).getUser(user.get().getUserId())).slash("expenses").slash(expense.getId()).toUri()).build();
 	}
 }
--- ExpenseManager/src/main/resources/Data.sql
+++ ExpenseManager/src/main/resources/Data.sql
@@ -2,6 +2,6 @@ INSERT INTO users(user_id,user_name,opening_amount,email,password,doj) VALUES (1
 INSERT INTO users(user_id,user_name,opening_amount,email,password,doj) VALUES (1002,'Vignesh bandla',6145,'vignesh6145@gmail.com','password',CURRENT_TIMESTAMP());
 INSERT INTO users(user_id,user_name,opening_amount,email,password,doj) VALUES (1003,'Vignesh Bandla VIT-AP',6145,'vignesh.19bce7085@vitap.ac.in@gmail.com','password',CURRENT_TIMESTAMP());
 
-INSERT INTO expense(id,description,user_user_id,amount) VALUES(1,'Vijayawada Petrol',1001,1000);
-INSERT INTO expense(id,description,user_user_id,amount) VALUES(2,'Hyderabad Petrol',1001,2000);
-INSERT INTO expense(id,description,user_user_id,amount) VALUES(3,'Chennai Petrol',1001,3000);
+INSERT INTO expense(id,description,user_user_id,amount) VALUES(1001,'Vijayawada Petrol',1001,1000);
+INSERT INTO expense(id,description,user_user_id,amount) VALUES(1002,'Hyderabad Petrol',1001,2000);
+INSERT INTO expense(id,description,user_user_id,amount) VALUES(1003,'Chennai Petrol',1001,3000);
